fd7b8ed1326ca6ff74948003971109a0bcbb3212
Nathaniel Okun <nokun@stanford.edu>
2017-01-31 15:44:24 -0800

    removed django code

--- Django_Experiment/NewsProcessorServer/ClientSideCode/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ClientsidecodeConfig(AppConfig):
-    name = 'ClientSideCode'
--- Django_Experiment/NewsProcessorServer/ClientSideCode/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def indexPage(request):
-    return HttpResponse("Hello, world. You're at the index.")
