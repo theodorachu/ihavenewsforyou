8df5fb8add1a0936b1d8f155e53312163f6dded7
Brandon Solis <solis17@stanford.edu>
2017-02-06 22:55:44 -0800

    Extension succesfully reads index page

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,36 +1,122 @@
-// Event listener for clicks on links in a browser action popup.
-// Open the link in a new tab of the current window.
-function onAnchorClick(event) {
-  chrome.tabs.create({ url: event.srcElement.href });
-  return false;
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-//TODO: apostrophe's
-function buildDummySites(){
-	var dummySites = [
-	{url:"http://www.nationalreview.com/article/444370/donald-trump-refugee-executive-order-no-muslim-ban-separating-fact-hysteria",
-	title:"Trumps Executive Order on Refugees, Separating Fact from Hysteria"},
-	{url:"https://www.wsj.com/articles/trump-blames-protesters-delta-sen-charles-schumer-for-airport-chaos-1485781646",
-	title:"White House Rebuffs Criticism of Travel Ban"}
-	]
-	return dummySites;
+/**
+ * @param {string} searchTerm - Search term for Google Image search.
+ * @param {function(string,number,number)} callback - Called when an image has
+ *   been found. The callback gets the URL, width and height of the image.
+ * @param {function(string)} errorCallback - Called when the image is not found.
+ *   The callback gets a string that describes the failure reason.
+ */
+function getImageUrl(searchTerm, callback, errorCallback) {
+  // Google image search - 100 searches per day.
+  // https://developers.google.com/image-search/
+  var searchUrl = 'https://across-the-aisle.herokuapp.com/';
+  //do this eventually
+//  + '?v=1.0&q=' + encodeURIComponent(searchTerm);
+  var x = new XMLHttpRequest();
+  x.open('GET', searchUrl);
+  // The Google image search API responds with JSON, so let Chrome parse it.
+  //x.responseType = 'json'; //will build this out on backend
+  x.responseType = 'text';
+  
+  x.onload = function() {
+    // Parse and process the response from Google Image Search.
+    var response = x.response;
+    /*
+    if (!response || !response.responseData || !response.responseData.results ||
+        response.responseData.results.length === 0) {
+      errorCallback('No response from Google Image search!');
+      return;
+    }
+    */
+    callback(response);
+  };
+  x.onerror = function() {
+    errorCallback('Network error');
+  };
+  x.send();
 }
 
-// Given an array of URLs, build a DOM list of these URLs in the
-// browser action popup.
-function buildPopupDom(realSites) {
-  var dummySites = buildDummySites();
-  var popupDiv = document.getElementById('suggested_div');
-  var ol = popupDiv.appendChild(document.createElement('ol'));
-
-  for (var i = 0; i < dummySites.length; i++) {
-    var li = ol.appendChild(document.createElement('li'));
-    var a = li.appendChild(document.createElement('a'));
-    a.href = dummySites[i].url;
-    a.appendChild(document.createTextNode(dummySites[i].title));
-    a.addEventListener('click', onAnchorClick);
-  }
+function renderStatus(statusText) {
+  //document.getElementById('status').textContent = statusText;
 }
 
-//builDummySites(buildPopUpDom);
-chrome.topSites.get(buildPopupDom);
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    // Put the image URL in Google search.
+    renderStatus('Performing Google Image search for ' + url);
+
+    getImageUrl(url, function(response) {
+
+      renderStatus('Search term: ' + url + '\n' +
+          'Google image search result: ' + response);
+      
+      //build dummy sites
+      var dummySites = [
+      {url:"http://www.nationalreview.com/article/444370/donald-trump-refugee-executive-order-no-muslim-ban-separating-fact-hysteria",
+      title:response}
+      ]
+      var popupDiv = document.getElementById('suggested_div');
+      var ol = popupDiv.appendChild(document.createElement('ol'));
+
+      for (var i = 0; i < dummySites.length; i++) {
+        var li = ol.appendChild(document.createElement('li'));
+        var a = li.appendChild(document.createElement('a'));
+        a.href = dummySites[i].url;
+        a.appendChild(document.createTextNode(dummySites[i].title));
+        a.addEventListener('click', onAnchorClick);
+      }
+      //and ignore them
+
+    }, function(errorMessage) {
+      renderStatus('Cannot display image. ' + errorMessage);
+    });
+  });
+});
