6f1161617386db83643b1e0768003ca50bc0b588
Kenneth Xu <kennethxu@rescomp-16-315108.stanford.edu>
2017-02-16 03:03:18 -0800

    Hopefully done debugging

--- NewsProcessorServer/server/views.py
+++ NewsProcessorServer/server/views.py
@@ -47,14 +47,14 @@ def visits():
 		return createJSONResp(error="Missing temporal field")	
 
 	visits = Visit.query.all() # TODO: Filter by date
-	results = [v.url for v in visits]
-	# for v in visits:
-		# a = Article.query.filter(url=str(v.url))
-		# results.append(dict(
-			# source=a.source,
-			# title=a.title,
-			# url=a.url
-			# ))
+	results = []
+	for v in visits:
+		a = Article.query.filter(url == v.url).first()
+		results.append(dict(
+			source=a.source,
+			title=a.title,
+			url=a.url
+			))
 	return json.dumps(results)
 
 @app.route('/recommend_articles', methods=['GET'])
